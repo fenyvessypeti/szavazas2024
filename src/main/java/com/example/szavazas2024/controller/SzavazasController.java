9013bc80c8b584397776528e082538746eb5963c
Fenyvessy Péter <fenyvessy.peti@gmail.com>
2024-01-07 22:25:46 +0100

    SzavazasController letrehozSzavazas, SzavazasService mentSzavazas metódusok létrehozása.

--- src/main/java/com/example/szavazas2024/controller/SzavazasController.java
+++ src/main/java/com/example/szavazas2024/controller/SzavazasController.java
@@ -1,8 +1,13 @@
 package com.example.szavazas2024.controller;
 
+import com.example.szavazas2024.dto.SzavazasCommand;
 import com.example.szavazas2024.service.SzavazasService;
 import com.example.szavazas2024.service.SzavazatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +22,16 @@ public class SzavazasController {
         this.szavazasService = szavazasService;
         this.szavazatService = szavazatService;
     }
+
+    @PostMapping("/szavazas")
+    public ResponseEntity<String> letrehozSzavazas(@RequestBody SzavazasCommand szavazasCommand) {
+        System.out.println("szavazasCommand: " + szavazasCommand);
+        try {
+            szavazasService.mentSzavazas(szavazasCommand);
+            return new ResponseEntity<>("Szavazás létrehozása sikeres", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Hiba történt a Szavazás létrehozása közben", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- src/main/java/com/example/szavazas2024/service/SzavazasService.java
+++ src/main/java/com/example/szavazas2024/service/SzavazasService.java
@@ -1,9 +1,15 @@
 package com.example.szavazas2024.service;
 
+import com.example.szavazas2024.domain.Szavazas;
+import com.example.szavazas2024.domain.Szavazat;
+import com.example.szavazas2024.dto.SzavazasCommand;
+import com.example.szavazas2024.dto.SzavazatCommand;
 import com.example.szavazas2024.repository.SzavazasRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -16,5 +22,24 @@ public class SzavazasService {
         this.szavazasRepository = szavazasRepository;
         this.szavazatService = szavazatService;
     }
+
+    public void mentSzavazas(SzavazasCommand szavazasCommand) {
+        Szavazas szavazas = new Szavazas();
+        szavazas.setIdopont(szavazasCommand.getIdopont());
+        szavazas.setTargy(szavazasCommand.getTargy());
+        szavazas.setTipus(szavazasCommand.getTipus());
+        szavazas.setElnok(szavazasCommand.getElnok());
+            szavazasRepository.save(szavazas);
+
+            for (SzavazatCommand szavazatCommand : szavazasCommand.getSzavazatok()) {
+                Szavazat szavazat = new Szavazat();
+                szavazat.setKepviselo(szavazatCommand.getKepviselo());
+                szavazat.setSzavazat(szavazatCommand.getSzavazat());
+                Szavazat mentettSzavazat = szavazatService.mentSzavazat(szavazat);
+                szavazat.setSzavazas(szavazas);
+            }
+
+    }
+
 }
 
