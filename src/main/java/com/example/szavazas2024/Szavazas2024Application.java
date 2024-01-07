c0089f8f01a51ae1338577a972b4362b1823099d
Fenyvessy Péter <fenyvessy.peti@gmail.com>
2024-01-07 20:28:04 +0100

    Szavazás projekt létrehozva.

---
+++ src/main/java/com/example/szavazas2024/Szavazas2024Application.java
@@ -0,0 +1,13 @@
+package com.example.szavazas2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Szavazas2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Szavazas2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/szavazas2024/Szavazas2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szavazas2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Szavazas2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
